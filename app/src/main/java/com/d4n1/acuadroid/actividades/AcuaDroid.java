04cbf9081d268aaeb28873f78e0371f78f556e2d
dmarti15 <dmarti15@gmail.com>
2016-01-18 17:13:50 +0100

    Add: Twitter opcion 1

--- app/src/main/java/com/d4n1/acuadroid/actividades/AcuaDroid.java
+++ app/src/main/java/com/d4n1/acuadroid/actividades/AcuaDroid.java
@@ -1,5 +1,6 @@
 package com.d4n1.acuadroid.actividades;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -21,8 +22,13 @@ import android.widget.TextView;
 
 import com.d4n1.acuadroid.R;
 import com.d4n1.acuadroid.auxiliares.TimeChecker;
+import com.d4n1.acuadroid.auxiliares.TweetHelper;
+import com.d4n1.acuadroid.auxiliares.TwitterWebActivity;
+import com.d4n1.acuadroid.auxiliares.Twitter_Statics;
 import com.d4n1.acuadroid.dialogos.ManLuxA;
 import com.d4n1.acuadroid.dialogos.ManLuxB;
+
+import twitter4j.auth.RequestToken;
 
 public class AcuaDroid extends AppCompatActivity implements
         ManLuxA.LuxADialogListener, ManLuxB.LuxBDialogListener {
@@ -32,12 +38,12 @@ public class AcuaDroid extends AppCompatActivity implements
 
     //Variables de estado de Acuadroid
     private int LuxA, LuxB, Temp, BatteryLevel;
-    static final String Status_LuxA="StatusLuxA",
-            Status_LuxB="StatusLuxB",
-            Status_Temp="StatusTemp",
-            Status_BatteryLevel="StatusBattery",
-            Status_ManTimer="StatusManTimer",
-            Status_Man="StatusMan";
+    static final String Status_LuxA = "StatusLuxA",
+            Status_LuxB = "StatusLuxB",
+            Status_Temp = "StatusTemp",
+            Status_BatteryLevel = "StatusBattery",
+            Status_ManTimer = "StatusManTimer",
+            Status_Man = "StatusMan";
 
     TextView txStatusA, txStatusB, txTemp;
     ProgressBar progressBar;
@@ -48,7 +54,16 @@ public class AcuaDroid extends AppCompatActivity implements
     Thread t;
     private boolean isMan;
     private int ManTimer;
-    SharedPreferences sharedPref ;
+    SharedPreferences sharedPref;
+
+
+    Twitter_Statics ts = new Twitter_Statics();
+    private RequestToken mRequestToken = null;
+
+    private TweetHelper tweet_hlp;
+    private int TWITTER_AUTH=1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +72,14 @@ public class AcuaDroid extends AppCompatActivity implements
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         isMan = false;
-        ManTimer=0;
+        ManTimer = 0;
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Creo instancia a clase para interactuar con twitter.
+        tweet_hlp = new TweetHelper(this);
+        //Verificamos que halla alguna key guardada en el telefono si no
+        //llamamos a un webview para obtenerlas
+        validar_login();
 
         setSupportActionBar(toolbar);
 
@@ -70,8 +91,29 @@ public class AcuaDroid extends AppCompatActivity implements
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, R.string.txt_FloatingButton, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try {
+                    //validamos login de nuevo
+                    validar_login();
+                    // obtenemos texto de componente de texto
+                    String texto = "Tweet de prueba de la nieva version de AcuadRoid";
+                    // enviamos tweet y obtenemos estado de envio
+                    boolean twt_snd_status = tweet_hlp.Send_Tweet(texto);
+                    if (twt_snd_status)// OK enviado
+                    { // cambiamos imagen de estad a twitter_OK
+                        Snackbar.make(view, "Tu tweet ha sido enviado", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                    } else {
+                        // cambiamos imagen de estad a twitter_fial
+                        Snackbar.make(view, "Hubo un Error no se pudo eviar tu tweet.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                    }
+
+                    Snackbar.make(view, R.string.txt_FloatingButton, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -85,7 +127,7 @@ public class AcuaDroid extends AppCompatActivity implements
         txStatusB.setTextColor(getResources().getColor(R.color.LuxB));
         txTemp = (TextView) findViewById(R.id.txTemp);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(Integer.valueOf(sharedPref.getString("TiempoManual", "10"))*60);
+        progressBar.setMax(Integer.valueOf(sharedPref.getString("TiempoManual", "10")) * 60);
 
         t = new Thread() {
             @Override
@@ -105,7 +147,7 @@ public class AcuaDroid extends AppCompatActivity implements
                 }
             }
         };
-        if(!t.isAlive()){
+        if (!t.isAlive()) {
             Log.d("AcuaDroid", "Arranca Ticker");
             t.start();
         }
@@ -173,90 +215,88 @@ public class AcuaDroid extends AppCompatActivity implements
     }
 
 
-
     @Override
     protected void onResume() {
         super.onResume();
         RunTimeChequer();
     }
 
-    public void ResfreshScreen(){
+    public void ResfreshScreen() {
         txStatusA.setText(sLuxA());
         txStatusB.setText(sLuxB());
         txTemp.setText(sTemp());
-        int fA=sharedPref.getInt("FaseA", -1);
-        int fB=sharedPref.getInt("FaseB", -1);
-        int fX=-1;
-        if(fA==2 && fB==2) fX=2;
-        else if (fA==1 || fB==1 || fA==2) fX=1;
-        else if (fA==3 || fB==3 || fB==2) fX=3;
-        else if (fA==0 && fB==0) fX=0;
+        int fA = sharedPref.getInt("FaseA", -1);
+        int fB = sharedPref.getInt("FaseB", -1);
+        int fX = -1;
+        if (fA == 2 && fB == 2) fX = 2;
+        else if (fA == 1 || fB == 1 || fA == 2) fX = 1;
+        else if (fA == 3 || fB == 3 || fB == 2) fX = 3;
+        else if (fA == 0 && fB == 0) fX = 0;
 
-        switch (fX){
+        switch (fX) {
             case 0:
-                if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    RelLay.setBackgroundResource (R.drawable.noche);
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    RelLay.setBackgroundResource(R.drawable.noche);
                 } else {
                     RelLay.setBackgroundColor(getResources().getColor(R.color.cNoche));
                 }
-                Fase=getResources().getString(R.string.Fase0);
+                Fase = getResources().getString(R.string.Fase0);
                 break;
             case 1:
-                if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    RelLay.setBackgroundResource (R.drawable.amanecer);
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    RelLay.setBackgroundResource(R.drawable.amanecer);
                 } else {
                     RelLay.setBackgroundColor(getResources().getColor(R.color.cAmanecer));
                 }
-                Fase=getResources().getString(R.string.Fase1);
+                Fase = getResources().getString(R.string.Fase1);
                 break;
             case 2:
-                if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    RelLay.setBackgroundResource (R.drawable.dia);
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    RelLay.setBackgroundResource(R.drawable.dia);
                 } else {
                     RelLay.setBackgroundColor(getResources().getColor(R.color.cDia));
                 }
-                Fase=getResources().getString(R.string.Fase2);
+                Fase = getResources().getString(R.string.Fase2);
                 break;
             case 3:
-                if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    RelLay.setBackgroundResource (R.drawable.amanecer);
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    RelLay.setBackgroundResource(R.drawable.amanecer);
                 } else {
                     RelLay.setBackgroundColor(getResources().getColor(R.color.cAmanecer));
                 }
-                Fase=getResources().getString(R.string.Fase3);
+                Fase = getResources().getString(R.string.Fase3);
                 break;
             default:
                 RelLay.setBackgroundColor(getResources().getColor(R.color.cError));
-                Fase=getResources().getString(R.string.FaseX);
-                Log.d("AcuaDroid", Fase+":"+fX);
+                Fase = getResources().getString(R.string.FaseX);
+                Log.d("AcuaDroid", Fase + ":" + fX);
                 break;
         }
-        if(Temp<Integer.valueOf(sharedPref.getString("temp_min", "0")))
-        {
+        if (Temp < Integer.valueOf(sharedPref.getString("temp_min", "0"))) {
             txTemp.setTextColor(ContextCompat.getColor(this, R.color.colorCold));
             //Log.d("AcuaDroid", "Hace frio "+AcuaDroidStatus.getTemp()+">"+sharedPref.getString("temp_min", "0"));
-        }else if(Temp>Integer.valueOf(sharedPref.getString("temp_max", "0"))){
+        } else if (Temp > Integer.valueOf(sharedPref.getString("temp_max", "0"))) {
             txTemp.setTextColor(ContextCompat.getColor(this, R.color.colorHot));
             //Log.d("AcuaDroid", "Hace calor");
-        }else {
+        } else {
             txTemp.setTextColor(ContextCompat.getColor(this, R.color.colorWarm));
             //Log.d("AcuaDroid", "Hace bueno");
         }
         progressBar.setProgress(ManTimer);
     }
 
-    public void UpdateAcuaDroidStatus(){
-        if(isMan){
-            if(ManTimer>0){
+    public void UpdateAcuaDroidStatus() {
+        if (isMan) {
+            if (ManTimer > 0) {
                 ManTimer--;
-                Log.d("AcuaDroid", "Modo Manual: " + ManTimer+ "sec ");
-            }else{
+                Log.d("AcuaDroid", "Modo Manual: " + ManTimer + "sec ");
+            } else {
                 SetManOff();
             }
-        }else{
-            LuxA=sharedPref.getInt("statusA", 0);
-            LuxB=sharedPref.getInt("statusB", 0);
-      //      AcuaDroidStatus.setBatteryLevel(sharedPref.getInt("Temp", 0));
+        } else {
+            LuxA = sharedPref.getInt("statusA", 0);
+            LuxB = sharedPref.getInt("statusB", 0);
+            //      AcuaDroidStatus.setBatteryLevel(sharedPref.getInt("Temp", 0));
             //Log.d("AcuaDroid", "Modo Auto");
         }
         ResfreshScreen();
@@ -276,60 +316,85 @@ public class AcuaDroid extends AppCompatActivity implements
     }
 
 
-
-    private void RunTimeChequer(){
+    private void RunTimeChequer() {
         Intent i = new Intent(this, TimeChecker.class);
         startService(i);
     }
-    private void StopTimeChequer(){
+
+    private void StopTimeChequer() {
         stopService(new Intent(this, TimeChecker.class));
     }
 
 
     @Override
     public void onPossitiveLuxAButtonClick(int pow) {
-        Log.d("AcuaDroid", "Modo Manual Azul MA: " + pow+ "pow ");
-        LuxA=pow;
+        Log.d("AcuaDroid", "Modo Manual Azul MA: " + pow + "pow ");
+        LuxA = pow;
         SetManOn();
         txStatusA.setText(sLuxA());
     }
+
     @Override
     public void onPossitiveLuxBButtonClick(int pow) {
-        LuxB=pow;
+        LuxB = pow;
         SetManOn();
         txStatusB.setText(sLuxB());
     }
 
 
-    public void SetManOn(){
-        if(!isMan)
-        {
-            ManTimer=Integer.valueOf(sharedPref.getString("TiempoManual", "10"))*60;
-            isMan=true;
+    public void SetManOn() {
+        if (!isMan) {
+            ManTimer = Integer.valueOf(sharedPref.getString("TiempoManual", "10")) * 60;
+            isMan = true;
             StopTimeChequer();
             Log.d("AcuaDroid", "Modo Manual ON");
         }
     }
-    public void SetManOff(){
-        if(isMan)
-        {
-            ManTimer=0;
-            isMan=false;
+
+    public void SetManOff() {
+        if (isMan) {
+            ManTimer = 0;
+            isMan = false;
             RunTimeChequer();
             Log.d("AcuaDroid", "Modo Manual OFF");
         }
     }
 
-    String sLuxA(){
-        return LuxA+getString(R.string.PercentSign);
-    }
-    String sLuxB(){
-        return LuxB+getString(R.string.PercentSign);
-    }
-    String sTemp(){
-        return Temp+getString(R.string.grados);
+    String sLuxA() {
+        return LuxA + getString(R.string.PercentSign);
     }
 
+    String sLuxB() {
+        return LuxB + getString(R.string.PercentSign);
+    }
 
+    String sTemp() {
+        return Temp + getString(R.string.grados);
+    }
 
+    public void validar_login(){
+        //Verificar si existen claves alamacenadas en el telefono si no llamar al webview.
+        if(!tweet_hlp.verify_logindata())
+        {
+            //Show_Toast("No se han encontrado claves en el telefono las descargaremos.");
+            //creamos intent y pasamos get_AuthenticationURL
+            Intent i = new Intent(AcuaDroid.this, TwitterWebActivity.class);
+            i.putExtra("URL", tweet_hlp.get_AuthenticationURL());
+            startActivityForResult(i, TWITTER_AUTH);
+
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //si el codigo obtenido es ok
+        if (resultCode == Activity.RESULT_OK) {
+            // Obtenemos oauth_verifier pasado por el webview
+            String oauthVerifier = (String) data.getExtras().get("oauth_verifier");
+            Log.e("oauthVerifier ->", oauthVerifier);
+
+            // Grabamos el valor de oauthVerifier en el shared preferences
+            tweet_hlp.Store_OAuth_verifier(oauthVerifier);
+
+        }
+
+    }
 }
---
+++ app/src/main/java/com/d4n1/acuadroid/auxiliares/Twitter_Statics.java
@@ -0,0 +1,23 @@
+package com.d4n1.acuadroid.auxiliares;
+
+/**
+ * Created by dmartinm on 18/01/2016.
+ */
+public class Twitter_Statics {
+    // TWITTER
+    public final static String OAUTH_CONSUMER_KEY = "uv1RAdGZFDFtfzNn7C75g";
+    public final static String OAUTH_CONSUMER_SECRET = "X9XZ6fC3RY7yBeCkDiMZBrXTf1ytuYQLR0jAC6e0kQ4";
+    public final static String TWITTER_CALLBACK = "twitter-callback:///";
+
+
+    // Shared Preferences Name
+    public static final String SHARED_PREF_NAME = "tweet_tst_App";
+
+    // Shared Preferences Name RecordS.
+    public static final String TW_ACCTOKEN = "OAUTH_CONSUMER_KEY";
+    public static final String TW_ACCTOKEN_SECRET = "OAUTH_CONSUMER_SECRET";
+ //  public static final String TW_ACCTOKEN = "2306510168-zegesyy556HertBMYss36fcUJt6aGA8yuomVzB4";
+ //  public static final String TW_ACCTOKEN_SECRET = "H5YcE7sPPBTGkf1X0qfGEEw9wdMOWnIwgKpwWClz477Wn";
+
+
+}
